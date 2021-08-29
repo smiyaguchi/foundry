ede705725c419c7648361c448c04c52382ef315a
smiyaguchi <smiyaguchi96@gmail.com>
2021-08-29 09:43:19 +0900

    Refector

--- internal/gen/default.go
+++ internal/gen/default.go
@@ -35,16 +35,13 @@ func (g *genDefault) Generate(option GenOption) (interface{}, error) {
 func randomStr(digit int32) (string, error) {
 	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-	// 乱数を生成
 	b := make([]byte, digit)
 	if _, err := rand.Read(b); err != nil {
 		return "", errors.New("unexpected error...")
 	}
 
-	// letters からランダムに取り出して文字列を生成
 	var result string
 	for _, v := range b {
-		// index が letters の長さに収まるように調整
 		result += string(letters[int(v)%len(letters)])
 	}
 	return result, nil
